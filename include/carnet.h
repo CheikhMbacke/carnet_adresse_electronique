1602827164da19cde75a8c9afbc158060a141c3a
cheikmbacke <serignecheikhmbackethiaw@gmail.com>
2019-05-02 04:00:11 +0000

    Mis a jour des fonctions

--- include/carnet.h
+++ include/carnet.h
@@ -2,42 +2,65 @@
 #ifndef __CARNET__H__
 #define __CARNET__H__
 
-/**/
+/**
+ * Structure personne 
+*/
 typedef struct Personne{
     char *nom;
     char *numero;
     struct Personne *next;
 }Personne ,*P;
 
-/**/
+/**
+ * Structure carnet
+*/
 typedef struct Carnet{
     P Tab;
     int compteur;
 }Carnet;
 
-/*Definition de fonction*/
+/********************************Definition de fonction**********************************/
 
-/* Instancier une personne*/
+/**
+ * Instancier une personne
+*/
 P create_person(char *name, char *num,P p );
 
-/*Afficher le nom et le numero d'une personne*/
+/**
+ * Afficher le nom et le numero d'une personne
+*/
 void print_person(Personne p);
 
-/*Initialiser le compteur du carnet*/
+/**
+ * Initialiser le compteur du carne
+*/
 void init_carnet(Carnet *c);
 
-/*Enregistrer une personne dans le carnet*/
+/**
+ * Enregistrer une personne dans le carnet
+*/
 void add_person(Carnet *c,P p);
 
-/*Afficher les enregistrements du carnet*/
+/**
+ * Afficher les enregistrements du carnet
+*/
 void print_carnet(Carnet c);
 
-/*Trier le carnet par odre alphabetique*/
+/**
+ * Trier le carnet par odre alphabetique
+*/
 void Tri_Alpha(Carnet *c);
 
 /*Sauvegarde de donnees*/
 void save_data(FILE *fic,Carnet c);
+/**
+ * Verifie si le fichier contient des elements
+*/
 int is_empty_file(FILE *file);
+/**
+ *Calcul le nombre de lign ed'un fichier
+*/
+int lenght_file(FILE *fic);
 /**
  * Restaurer les donnees dans le carnet
 */
--- src/carnet.c
+++ src/carnet.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include "../include/carnet.h"
 
-/* Instancier une personne*/
+/**
+ * Instancier une personne
+*/
   P create_person(char *name, char *num,P p){
     P person;
     person = malloc(sizeof(Personne));
@@ -14,28 +16,34 @@
     person->next = p;
     return person;
 }
-
-/*Afficher le nom et le numero d'une personne*/
+/**
+ * Afficher le nom et le numero d'une personne
+*/
 void print_person(Personne p){
     printf("Nom complet: %s\n",p.nom);
     printf("Numéro: %s\n",p.numero);
     //printf("suivant: %p\n",p.next);
 }
-
-/*Initialiser le compteur du carnet*/
+/**
+ * Initialiser le compteur du carnet
+*/
 void init_carnet(Carnet *c){
      c->compteur = 0;
 }
 
-/*Enregistrer une personne dans le carnet*/
+/**
+ * Enregistrer une personne dans le carnet
+*/
 void add_person(Carnet *c,P p){
     if(c->compteur == 0)
-        c->Tab = malloc(sizeof(Personne));
+        c->Tab = malloc(2*sizeof(Personne));
     c->Tab[c->compteur] = *p;
     c->compteur++;
 }
 
-/*Afficher les enregistrements du carnet*/
+/**
+ * Afficher les enregistrements du carnet
+*/
 void print_carnet(Carnet c){
     if((c.compteur) == 0){
         printf("Le carnet est vide\n");
@@ -50,26 +58,54 @@ void print_carnet(Carnet c){
  /* /*Trier le carnet par odre alphabetique*/
 void Tri_Alpha(Carnet *c){
     int i,j;//INDICE DE PARCOURS DU TABLEAU,
-    char *cle = (char *)malloc(sizeof(char)); //variable tampon
+    P cle = malloc(sizeof(Personne)); //variable tampon
 
     for(i = 1; i < c->compteur; i++)
     {
-        strcpy(cle,c->Tab[i].nom);
+        //strcpy(cle,c->Tab[i].nom);
+        *cle = c->Tab[i];
         j = i-1;
-        while(j>=0 && (strcmp(c->Tab[j].nom,cle) > 0)){
-            strcpy(c->Tab[j+1].nom,c->Tab[j].nom);
-            strcpy(c->Tab[j+1].numero,c->Tab[j].numero);
+        while(j>=0 && (strcmp(c->Tab[j].nom,cle->nom) > 0)){
+            /* strcpy(c->Tab[j+1].nom,c->Tab[j].nom);
+            strcpy(c->Tab[j+1].numero,c->Tab[j].numero); */
+            c->Tab[j+1] = c->Tab[j];
             j--;
         }
-        strcpy(c->Tab[j+1].nom,cle);
-        strcpy(c->Tab[j+1].numero,c->Tab[i].numero);
+        /* strcpy(c->Tab[j+1].nom,cle);
+        strcpy(c->Tab[j+1].numero,c->Tab[i].numero); */
+        c->Tab[j+1] = *cle;
     }
 } 
+/**
+ * Trier le carnet par odre alphabetique
+*/
+/* void Tri_Alpha(Carnet *c){
+    int i = 0,test;
+    P person = malloc(sizeof(Personne));
+    do
+    {
+        test = 0;
+        for( int j = c->compteur -1; j > i ; j--)
+        {
+            if (strcmp(c->Tab[j].nom,c->Tab[j-1].nom) <0) {
+                *person = c->Tab[j];
+                c->Tab[j] = c->Tab[j-1];
+                c->Tab[j-1] = *person;
+                test = 1;
+            }
+            
+        }
+        
+    } while (test == 1 && i< c->compteur);
+    
+}  */
 
-/*Sauvegarde de donnees*/
+/**
+ * Sauvegarde de donnees
+*/
 void save_data(FILE *fic,Carnet c){
 
-    fic=fopen("saveFile.txt","a+");
+    fic=fopen("saveFile.txt","w");
     for(int i = 0; i < c.compteur; i++){
             fprintf(fic,"%s %s\n",c.Tab[i].nom,c.Tab[i].numero);
         }
@@ -84,6 +120,17 @@ int is_empty_file(FILE *file){
         return 0;
     return 1;   
 }
+int lenght_file(FILE *fic){
+    if (fic == NULL) {
+        fprintf(stderr,"Erreur d'ouverture du fichier\n");
+        exit(0);
+    }
+    int length = 0;
+    char *mot;
+    while(fgets(mot,255,fic) != NULL)
+        length++;
+    return length;
+}
 void restaure_data(FILE *fic, Carnet *c){
     fic = fopen("saveFile.txt","r");
     P person = NULL;
@@ -97,14 +144,13 @@ void restaure_data(FILE *fic, Carnet *c){
     if (is_empty_file(fic)) {
         rewind(fic);
         while(1){
-            if (feof(fic)){
-                return;
-            }else
-            {
+                //printf("taille:%d",lenght_file(fic));
                 fscanf(fic,"%s %s",nom,num);
                 person = create_person(nom,num,person);
                 add_person(c,person);
-            }
+            if (feof(fic))
+                return;
+        
         }
     free(nom);
     free(num);
